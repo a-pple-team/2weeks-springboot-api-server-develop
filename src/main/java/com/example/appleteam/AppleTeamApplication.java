451b178b1d7b7072cf2a0c29ca69fdc893f56302
신희성 <vvwv3092@gmail.com>
2024-05-13 10:30:01 +0900

    init

---
+++ src/main/java/com/example/appleteam/AppleTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.appleteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appleteam/AppleTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appleteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
